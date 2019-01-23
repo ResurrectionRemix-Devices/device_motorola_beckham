d0804a3e9948c64d21d5548d7c9de0bfc32845ea
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-01-22 22:02:49 -0500

    beckham: RR :P
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,4 +15,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_beckham.mk
+    $(LOCAL_DIR)/rr_beckham.mk
--- rr_beckham.mk
+++ rr_beckham.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 #
 
-# Inherit some common Lineage stuff.
+# Inherit some common RR stuff.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/rr/config/common_full_phone.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
@@ -71,7 +71,7 @@ endif
 
 # Device identifiers
 PRODUCT_DEVICE := beckham
-PRODUCT_NAME := lineage_beckham
+PRODUCT_NAME := rr_beckham
 PRODUCT_BRAND := motorola
 PRODUCT_MODEL := Moto Z3 Play
 PRODUCT_MANUFACTURER := Motorola
